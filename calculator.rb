6071d1976e2dee7820bd2a15228d6f078f31a845
rabab raza ali <robin.ali85@yahoo.com>
2019-07-11 22:45:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number=1
 second_number=2
 sum= first_number + second_number
+difference = first_number - second_number
+product= first number x second_number
+quotient= first_number x second_number
